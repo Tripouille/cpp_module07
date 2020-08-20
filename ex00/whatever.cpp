597d5f17b735a262c49a6fcdb404535c380dd8cd
jgambard <jgambard@student.42lyon.fr>
2020-08-20 16:33:55 +0200

    fix ex00

--- ex00/whatever.cpp
+++ ex00/whatever.cpp
@@ -1,11 +1,32 @@
 #include <iostream>
 
+class
+Awesome {
+    public:
+    Awesome( int n ) : _n( n ) {}
+        bool operator==( Awesome const & rhs ) { return (this->_n == rhs._n); }
+        bool operator!=( Awesome const & rhs ) { return (this->_n != rhs._n); }
+        bool operator>( Awesome const & rhs ) { return (this->_n > rhs._n); }
+        bool operator<( Awesome const & rhs ) { return (this->_n < rhs._n); }
+        bool operator>=( Awesome const & rhs ) { return (this->_n >= rhs._n); }
+        bool operator<=( Awesome const & rhs ) { return (this->_n <= rhs._n); }
+
+        int     getN(void) const { return (_n); }
+    private:
+        int _n;
+};
+
+std::ostream &      operator<<(std::ostream & os, Awesome const & aw)
+{
+    os << aw.getN();
+    return (os);
+}
+
 template<typename T>
 void
 swap(T & a, T & b)
 {
-    T   tmp;
-    tmp = a;
+    T   tmp(a);
     a = b;
     b = tmp;
 }
@@ -14,14 +35,14 @@ template<typename T>
 T const &
 max(T const & a, T const & b)
 {
-    return (a > b ? a : b);
+    return (const_cast<T &>(a) > const_cast<T &>(b) ? a : b);
 }
 
 template<typename T>
 T const &
 min(T const & a, T const & b)
 {
-    return (a < b ? a : b);
+    return (const_cast<T &>(a) < const_cast<T &>(b) ? a : b);
 }
 
 int
@@ -47,7 +68,20 @@ main(void)
      std::cout << "TEST AVEC DOUBLE" << std::endl;
     double const d1 = 21.1;
     double const d2 = 42.1;
-    std::cout << "min(c, d) = " << ::min(d1, d2) << std::endl;
-    std::cout << "max(c, d) = " << ::max(d1, d2) << std::endl;
+    std::cout << "min(d1, d2) = " << ::min(d1, d2) << std::endl;
+    std::cout << "max(d1, d2) = " << ::max(d1, d2) << std::endl;
+    std::cout << "TEST AVEC Awesome" << std::endl;
+    Awesome a1 = 2;
+    Awesome a2 = 10;
+    std::cout << "min(a1, a2) = " << ::min(a1, a2) << std::endl;
+    std::cout << "max(a1, a2) = " << ::max(a1, a2) << std::endl;
+    std::cout << "a1 = " << a1 << ", a2 = " << a2 << std::endl;
+    ::swap(a1, a2);
+    std::cout << "a1 = " << a1 << ", a2 = " << a2 << std::endl;
+    std::cout << "TEST AVEC Awesome CONST" << std::endl;
+    Awesome const ac1 = 2;
+    Awesome const ac2 = 100;
+    std::cout << "min(ac1, ac2) = " << ::min(ac1, ac2) << std::endl;
+    std::cout << "max(ac1, ac2) = " << ::max(ac1, ac2) << std::endl;
     return 0;
 }
